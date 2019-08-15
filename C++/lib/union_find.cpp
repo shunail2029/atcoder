0e0f907ab962d032c38b968756162fe57d19ae97
shunail2029 <tiger1998nov27@gmail.com>
2019-08-16 00:19:10 +0900

    add union_find_cnt to union_find.cpp

--- C++/lib/union_find.cpp
+++ C++/lib/union_find.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 
 class union_find {
-    private :
+    protected :
         std::vector<int> par;
     public :
         union_find(int);
@@ -32,4 +32,34 @@ void union_find::unite(int x, int y) {
     if (x_par == y_par) return;
 
     par.at(x_par) = y_par;
+}
+
+class union_find_cnt : public union_find {
+    private :
+        std::vector<int> cnt;
+    public :
+        union_find_cnt(int);
+        int size(int);
+        void unite(int, int);
+};
+
+union_find_cnt::union_find_cnt(int n) : union_find(n) {
+    cnt.resize(n);
+    for (int i=0; i<n; i++) {
+        cnt.at(i) = 1;
+    }
+}
+
+int union_find_cnt::size(int x) {
+    return cnt.at(find(x));
+}
+
+void union_find_cnt::unite(int x, int y) {
+    int x_par = find(x);
+    int y_par = find(y);
+    if (x_par == y_par) return;
+
+    par.at(x_par) = y_par;
+    cnt.at(y_par) += cnt.at(x_par);
+    cnt.at(x_par) = 0;
 }
