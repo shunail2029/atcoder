fcfe691b781cb0cbf634481073365931e64ec370
shunail2029 <tiger1998nov27@gmail.com>
2019-09-07 22:40:27 +0900

    add template to binary_search

--- C++/lib/binary_search.cpp
+++ C++/lib/binary_search.cpp
@@ -1,7 +1,8 @@
 #include <vector>
 
 // binary_search 二分探索
-long long binary_searching(std::vector<long long> &str, long long key, long long imin, long long imax) {
+template<typename T>
+long long binary_searching(std::vector<T> &str, T key, long long imin, long long imax) {
     if (imax < imin) return -1;
     long long imid = imin + (imax - imin) / 2;
     if (str.at(imid) < key) {
