bb2cb9aaed5ab3c5a1d6949c8fb5b9d9ea289b77
shunail2029 <tiger1998nov27@gmail.com>
2019-09-28 12:04:37 +0900

    add lis.cpp

---
+++ C++/lib/lis.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <vector>
+
+// LIS(Longest Increasing Subsequence) 最長増加部分列
+int lis(std::vector<long long> &vec) {
+    int len = vec.size();
+    std::vector<long long> dp(len, 1e16);
+    for (int i=0; i<len; i++) {
+        long long tmp = vec.at(i);
+        auto iter = std::lower_bound(dp.begin(), dp.end(), tmp);
+        *iter = tmp;
+    }
+    return std::lower_bound(dp.begin(), dp.end(), 1e16) - dp.begin();
+}
