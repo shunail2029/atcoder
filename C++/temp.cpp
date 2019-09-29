eb657f4e265068602d698e0b2e2d3558adf4cf94
shunail2029 <tiger1998nov27@gmail.com>
2019-09-29 10:05:00 +0900

    add shortest_path

--- C++/temp.cpp
+++ C++/temp.cpp
@@ -13,6 +13,8 @@
 #include <string>
 #include <vector>
 
+using Graph = std::vector<std::vector<int>>;
+using WGraph = std::vector<std::vector<std::pair<int, long long>>>;
 using ll = long long;
 using namespace std;
 
