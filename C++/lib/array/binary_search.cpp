f99fd5ce053d8ad4080c506ed1c197daa918708d
shunail2029 <tiger1998nov27@gmail.com>
2019-09-29 10:54:06 +0900

    make directory

---
+++ C++/lib/array/binary_search.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+// binary_search 二分探索
+template<typename T>
+long long binary_searching(std::vector<T> &str, T key, long long imin, long long imax) {
+    if (imax < imin) return -1;
+    long long imid = imin + (imax - imin) / 2;
+    if (str.at(imid) < key) {
+        return binary_searching(str, key, imid+1, imax);
+    }
+    else if (str.at(imid) > key) {
+        return binary_searching(str, key, imin, imid-1);
+    }
+    else {
+        return imid;
+    }
+}
---
+++ C++/lib/integer/gcd.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+
+// greatest common divisor 最大公約数
+long long gcd(long long a, long long b) {
+    if (a < b) {
+        std::swap(a, b);
+    }
+    while (a%b > 0) {
+        long long buf = b;
+        b = a%b;
+        a = buf;
+    }
+    return b;
+}
+
+// extra GCD 拡張ユークリッドの互除法
+/* ax + by = gcd(a, b) の解をx,yに格納する
+   return value : gcd(a, b)
+   a > b を想定 */
+long long extGCD(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long d = extGCD(b, a%b, y, x);
+    y -= a / b * x;
+    return d;
+}
