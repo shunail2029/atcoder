cfa99b7efdaab6a9ad3ff71e2c5822ffc8f8f7c1
shunail2029 <tiger1998nov27@gmail.com>
2019-12-30 17:41:51 +0900

    rewrite unionfind

--- C++/lib/data_structure/union_find.cpp
+++ C++/lib/data_structure/union_find.cpp
@@ -1,66 +1,52 @@
 #include <vector>
 
-class union_find {
+class UnionFind {
     protected :
         std::vector<int> par;
     public :
-        union_find(int);
-        int find(int);
-        bool same(int, int);
-        virtual void unite(int, int);
+        UnionFind(int n) {
+            par.resize(n);
+            for (int i=0; i<n; i++) {
+                par.at(i) = i;
+            }
+        }
+        int find(int x) {
+            if (par.at(x) == x) return x;
+            else return par.at(x) = find(par.at(x));
+        }
+        bool same(int x, int y) {
+            return find(x) == find(y);
+        }
+        virtual void unite(int x, int y) {
+            int xPar = find(x);
+            int yPar = find(y);
+            if (xPar == yPar) return;
+
+            par.at(xPar) = yPar;
+        }
 };
 
-union_find::union_find(int n) {
-    par.resize(n);
-    for (int i=0; i<n; i++) {
-        par.at(i) = i;
-    }
-}
-
-int union_find::find(int x) {
-    if (par.at(x) == x) return x;
-    else return par.at(x) = find(par.at(x));
-}
-
-bool union_find::same(int x, int y) {
-    return find(x) == find(y);
-}
-
-void union_find::unite(int x, int y) {
-    int x_par = find(x);
-    int y_par = find(y);
-    if (x_par == y_par) return;
-
-    par.at(x_par) = y_par;
-}
-
-// union_find with count
-class union_find_cnt : public union_find {
+// Union-Find with count
+class UnionFindCnt : public UnionFind {
     private :
         std::vector<int> cnt;
     public :
-        union_find_cnt(int);
-        int size(int);
-        void unite(int, int);
+        UnionFindCnt(int n) : UnionFind(n) {
+            cnt.resize(n);
+            for (int i=0; i<n; i++) {
+                cnt.at(i) = 1;
+            }
+        }
+        int size(int x) {
+            return cnt.at(find(x));
+        }
+        void unite(int x, int y) {
+            int x_par = find(x);
+            int y_par = find(y);
+            if (x_par == y_par) return;
+
+            par.at(x_par) = y_par;
+            cnt.at(y_par) += cnt.at(x_par);
+            cnt.at(x_par) = 0;
+        }
 };
-
-union_find_cnt::union_find_cnt(int n) : union_find(n) {
-    cnt.resize(n);
-    for (int i=0; i<n; i++) {
-        cnt.at(i) = 1;
-    }
-}
-
-int union_find_cnt::size(int x) {
-    return cnt.at(find(x));
-}
-
-void union_find_cnt::unite(int x, int y) {
-    int x_par = find(x);
-    int y_par = find(y);
-    if (x_par == y_par) return;
-
-    par.at(x_par) = y_par;
-    cnt.at(y_par) += cnt.at(x_par);
-    cnt.at(x_par) = 0;
-}
