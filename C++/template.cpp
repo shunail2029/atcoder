9463287b24ce06529f69d11348f24e42809607bb
shunail2029 <tiger1998nov27@gmail.com>
2020-01-21 17:01:29 +0900

    fixed template.cpp

--- C++/template.cpp
+++ C++/template.cpp
@@ -6,14 +6,15 @@ using ll = long long;
 using Graph = vector<vector<int>>;
 using WGraph = vector<vector<pair<int, ll>>>;
 
-template<class T>bool chmax(T &a, const T &b) { if (b > a) { a = b; return true; } return false; }
-template<class T>bool chmin(T &a, const T &b) { if (b < a) { a = b; return true; } return false; }
+template<class T>inline bool chmax(T &a, const T &b) { if (b > a) { a = b; return true; } return false; }
+template<class T>inline bool chmin(T &a, const T &b) { if (b < a) { a = b; return true; } return false; }
 
 constexpr int dx[4] = {-1, 0, 0, 1}, dy[4] = {0, -1, 1, 0};
 constexpr int INF = 1e9;
 constexpr long long LINF = 1e18;
 constexpr long long MOD = 1e9 + 7;
 constexpr double EPS = 1e-10;
+constexpr double PI = M_PI;
 #pragma endregion
 
 void solve(){}
