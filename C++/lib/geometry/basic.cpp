70d95d4a26fa5ac2a4ce193c3cae173cbfaf050f
shunail2029 <tiger1998nov27@gmail.com>
2020-01-24 17:29:26 +0900

    fixed geometry/basic

--- C++/lib/geometry/basic.cpp
+++ C++/lib/geometry/basic.cpp
@@ -8,25 +8,28 @@ class Point {
     public:
         double x, y;
         Point(double xx = 0.0, double yy = 0.0) : x(xx), y(yy) {}
+        Point(const Point &p) : x(p.x), y(p.y) {}
         Point& operator=(const Point &p) { x = p.x; y = p.y; return *this; }
+        Point operator+() const { return Point(x, y); }
+        Point operator-() const { return Point(-x, -y); }
+        Point operator+(const Point &p) const { return Point(*this) += p; }
+        Point operator-(const Point &p) const { return Point(*this) -= p; }
+        Point operator*(double v) const { return Point(*this) *= v; }
+        Point operator/(double v) const { return Point(*this) /= v; }
+        Point& operator+=(const Point &p) { x += p.x; y += p.y; return *this; }
+        Point& operator-=(const Point &p) { x -= p.x; y -= p.y; return *this; }
+        Point& operator*=(double v) { x *= v; y *= v; return *this; }
+        Point& operator/=(double v) { x /= v; y /= v; return *this; }
+        friend Point operator*(double v, const Point &p) { return p * v; }
+        friend Point rot(const Point &p, double angle) { return Point(std::cos(angle) * p.x - std::sin(angle) * p.y, std::sin(angle) * p.x + std::cos(angle) * p.y); }
+        friend Point rot90(const Point &p) { return Point(-p.y, p.x); }
+        friend double dot(const Point &p, const Point &q) { return p.x * q.x + p.y * q.y; }
+        friend double abs(const Point &p) { return sqrt(dot(p, p)); }
+        friend double dis(const Point &p, const Point &q) { return abs(p - q); }
+        friend bool eq(const Point &p, const Point &q) { return dis(p, q) < EPS; }
+        friend std::istream& operator>>(std::istream &s, Point &p) { return s >> p.x >> p.y; }
+        friend std::ostream& operator<<(std::ostream &s, const Point &p) { return s << "(" << p.x << ", " << p.y << ")"; }
 };
-inline Point operator+(const Point &p) { return Point(p.x, p.y); }
-inline Point operator-(const Point &p) { return Point(-p.x, -p.y); }
-inline Point operator+(const Point &p, const Point &q) { return Point(p.x + q.x, p.y + q.y); }
-inline Point operator-(const Point &p, const Point &q) { return Point(p.x - q.x, p.y - q.y); }
-inline Point& operator+=(Point &p, const Point &q) { p.x += q.x; p.y += q.y; return p; }
-inline Point& operator-=(Point &p, const Point &q) { p.x -= q.x; p.y -= q.y; return p; }
-inline Point operator*(const Point &p, double v) { return Point(p.x * v, p.y * v); }
-inline Point operator*(double v, const Point &p) { return Point(p.x * v, p.y * v); }
-inline Point operator/(const Point &p, double v) { return Point(p.x / v, p.y / v); }
-inline Point rot(const Point &p, double angle) { return Point(std::cos(angle) * p.x - std::sin(angle) * p.y, std::sin(angle) * p.x + std::cos(angle) * p.y); }
-inline Point rot90(const Point &p) { return Point(-p.y, p.x); }
-inline double dot(const Point &p, const Point &q) { return p.x * q.x + p.y * q.y; }
-inline double abs(const Point &p) { return sqrt(dot(p, p)); }
-inline double dis(const Point &p, const Point &q) { return abs(p - q); }
-inline bool eq(const Point &p, const Point &q) { return dis(p, q) < EPS; }
-inline std::istream& operator>>(std::istream &s, Point &p) { return s >> p.x >> p.y; }
-inline std::ostream& operator<<(std::ostream &s, const Point &p) { return s << "(" << p.x << ", " << p.y << ")"; }
 
 class Circle {
     public:
@@ -36,7 +39,7 @@ class Circle {
         Circle(double cx, double cy, double rr) : center(Point(cx, cy)), r(rr) {}
         Circle(Point p, double rr) : center(p), r(rr) {}
         Circle& operator=(const Circle &c) { center = c.center; r = c.r; return *this; }
+        friend Circle mv(const Circle &c, double x, double y) { return Circle(c.center.x + x, c.center.y + y, c.r); }
+        friend bool eq(const Circle &c, const Circle &d) { return dis(c.center, d.center) < EPS && abs(c.r - d.r) < EPS; }
+        friend std::ostream& operator<<(std::ostream &s, const Circle &c) { return s << "(" << c.center.x << ", " << c.center.y << ", " << c.r << ")"; }
 };
-inline Circle mv(const Circle &c, double x, double y) { return Circle(c.center.x + x, c.center.y + y, c.r); }
-inline bool eq(const Circle &c, const Circle &d) { return dis(c.center, d.center) < EPS && abs(c.r - d.r) < EPS; }
-inline std::ostream& operator<<(std::ostream &s, const Circle &c) { return s << "(" << c.center.x << ", " << c.center.y << ", " << c.r << ")"; }
