11948fc3545a67d94097bea9ca5ff075ebe795b6
shunail2029 <tiger1998nov27@gmail.com>
2020-06-03 10:44:35 +0900

    add BinaryIndexedTreeRMQ

--- C++/lib/data_structure/binary_indexed_tree.cpp
+++ C++/lib/data_structure/binary_indexed_tree.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include <vector>
 
 // Binary Indexed Tree (BIT)
@@ -29,5 +30,55 @@ class BinaryIndexedTree {
         // a and b are 1-indexed
         T sum(int a, int b) const {
             return sum(b-1) - sum(a-1);
+        }
+};
+
+// Binary Indexed Tree RMQ (BIT)
+template<class T>
+class BinaryIndexedTreeRMQ {
+    private:
+        int N;
+        std::vector<T> dat, ind;
+    public:
+        BinaryIndexedTreeRMQ(int n, T def) : N(n), dat(n+1, def), ind(n+1) {
+            for (int i=1; i<n+1; ++i) ind.at(i) = i;
+        }
+        // return index of max of [l, r]
+        // l and r are 1-indexed
+        int queryInd(int l, int r) const {
+            int res = 0;
+            while (l <= r) {
+                if (r-(r&-r)+1 >= l) {
+                    if (dat.at(ind.at(r)) > dat.at(res)) res = ind.at(r);
+                    r -= (r & -r);
+                } else {
+                    if (dat.at(r) > dat.at(res)) res = r;
+                    --r;
+                }
+            }
+            return res;
+        }
+        // return max of [l, r]
+        // l and r are 1-indexed
+        T query(int l, int r) const {
+            return dat.at(queryInd(l, r));
+        }
+        // a is 1-indexed
+        void update(int a, const T v) {
+            dat.at(a) = v;
+            int cur = a;
+            while (cur <= N) {
+                if (ind.at(cur) == a) {
+                    int nx = queryInd(cur-(cur&-cur)+1, cur-1);
+                    ind.at(cur) = dat.at(cur) > dat.at(nx) ? cur : nx;
+                } else {
+                    if (dat.at(a) > dat.at(ind.at(cur))) ind.at(cur) = a;
+                }
+                cur += (cur & -cur);
+            }
+        }
+        // return value of a
+        T at(int a) const {
+            return dat.at(a);
         }
 };
