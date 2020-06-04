005583db9325fec599f361024d0a0e711665586e
shunail2029 <tiger1998nov27@gmail.com>
2020-06-05 00:35:25 +0900

    fix lcs

--- C++/lib/array/lcs.cpp
+++ C++/lib/array/lcs.cpp
@@ -3,14 +3,14 @@
 
 // LCS (Longest Common Subsequence) 最長共通部分列
 template<class T>
-long long lcs(const std::vector<T> &v1, const std::vector<T> &v2) {
+int lcs(const std::vector<T> &v1, const std::vector<T> &v2) {
     int l1 = (int)v1.size(), l2 = (int)v2.size();
-    std::vector<std::vector<long long>> dp(l1, std::vector<long long>(l2, 0LL));
+    std::vector<std::vector<int>> dp(l1, std::vector<int>(l2, 0));
 
     for (int i=0; i<l1; ++i) {
         for (int j=0; j<l2; ++j) {
-            if (v1.at(l1) == v2.at(l2)) {
-                dp.at(i+1).at(j+1) = dp.at(i).at(j);
+            if (v1.at(i) == v2.at(j)) {
+                dp.at(i+1).at(j+1) = dp.at(i).at(j) + 1;
             } else {
                 dp.at(i+1).at(j+1) = std::max(dp.at(i+1).at(j), dp.at(i).at(j+1));
             }
