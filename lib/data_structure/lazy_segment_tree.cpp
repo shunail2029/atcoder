b9f28eedbb1e5cbd08a0ebe084b56a9b125f1b05
shunail2029 <tiger1998nov27@gmail.com>
2020-07-05 22:45:44 +0900

    add LazySegmentTree(range add/range sum)

--- lib/data_structure/lazy_segment_tree.cpp
+++ lib/data_structure/lazy_segment_tree.cpp
@@ -59,3 +59,60 @@ class StarrySkyTree {
             return std::min(vleft, vright);
         }
 };
+
+// Lazy Segment Tree (区間加算・区間和)
+template<class T>
+class LazySegmentTree {
+    private:
+        int N;
+        const T ZERO;
+        std::vector<T> dat, lazy;
+    public:
+        LazySegmentTree(int n) : ZERO(static_cast<T>(0)) {
+            N = 1;
+            while (N < n) N *= 2;
+            dat.assign(2*N, ZERO);
+            lazy.assign(2*N, ZERO);
+        }
+        // a is 0-indexed
+        void set(int a, const T v) { dat[a+N] = v; }
+        void build() { for (int i=N-1; i>0; --i) dat[i] = dat[2*i] + dat[2*i+1]; }
+        void eval(int k, int l, int r) {
+            if (lazy[k] == ZERO) return;
+
+            dat[k] += lazy[k] * (r - l);
+            if (r - l > 1) {
+                lazy[2*k] += lazy[k];
+                lazy[2*k+1] += lazy[k];
+            }
+            lazy[k] = ZERO;
+        }
+        // add v to [a, b)
+        // a and b are 0-indexed
+        void add(int a, int b, const T v) { return add (a, b, v, 1, 0, N); }
+        void add(int a, int b, const T v, int k, int l, int r) {
+            eval(k, l, r);
+
+            if (b <= l || r <= a) return;
+            if (a <= l && r <= b) {
+                lazy[k] += v;
+                eval(k, l, r);
+            } else {
+                add(a, b, v, 2*k, l, (l+r)/2);
+                add(a, b, v, 2*k+1, (l+r)/2, r);
+                dat[k] = dat[2*k] + dat[2*k+1];
+            }
+        }
+        // get min of [a, b)
+        // a and b are 0-indexed
+        T get(int a, int b) { return get(a, b, 1, 0, N); }
+        T get(int a, int b, int k, int l, int r) {
+            eval(k, l, r);
+
+            if (b <= l || r <= a) return ZERO;
+            if (a <= l && r <= b) return dat[k];
+            T vleft = get(a, b, 2*k, l, (l+r)/2);
+            T vright = get(a, b, 2*k+1, (l+r)/2, r);
+            return vleft + vright;
+        }
+};
