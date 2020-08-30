895a1b288e898441e0adf540ef906225ff762404
shunail2029 <tiger1998nov27@gmail.com>
2020-08-30 18:24:23 +0900

    delete dx, dy

--- template.cpp
+++ template.cpp
@@ -8,7 +8,6 @@ using WGraph = vector<vector<pair<int, ll>>>;
 template<class T>inline bool chmax(T &a, const T &b) { if (b > a) { a = b; return true; } return false; }
 template<class T>inline bool chmin(T &a, const T &b) { if (b < a) { a = b; return true; } return false; }
 
-constexpr int dx[4] = {-1, 0, 0, 1}, dy[4] = {0, -1, 1, 0};
 constexpr int INF = 1e9;
 constexpr int MOD = 1e9 + 7;
 constexpr long long LINF = 1e18;
